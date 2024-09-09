5d0aa3f345d85863ff8fc348627001ef5344f2e1
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2024-09-09 10:56:39 -0700

    Implemented some error handling, and better control flow

--- Source/ExoFyle.py
+++ Source/ExoFyle.py
@@ -15,6 +15,8 @@ def Set_Background_Color(): Background["Color"] = Color(Background["Red"], Backg
 
 # Technically all of these are usable within in ExoFyle config files, and I'm not saying you should or shouldn', but uh, let's consider this a hazard sign#
 def Load_All_Packages():
+    # TO DO: yo, we can't allow for anyone to import anything else.
+    # I think that will be the easiest way to secure the api up front, and then I'll implement more along the way.
     print("Loading Packages")
     for PackageFile in listdir("Packages"):
         with open("Packages/" + PackageFile, 'r') as PackageFile:
@@ -30,6 +32,33 @@ def Load_All_Packages():
 def Is_Legal_Script(UserInstruction:str) -> bool:
     for Instruction in InvalidInstructions:
         if Instruction in UserInstruction:
+            return False
+    return True
+
+
+# We have to take into account the packages within the user's folder, and whatever changes they may make when live
+def Build_Frame():
+    begin_drawing()
+    clear_background(Background["Color"])
+    end_drawing()
+
+
+def Handle_Input() -> None | str:
+    for Key, Function, KeyChord in InputTree:
+        if is_key_pressed(Key):
+            if KeyChord != None:
+                if is_key_down(KeyChord) == False:
+                    return f"Command Requires KeyChord {KeyChord}"
+            Function()
+
+
+def Handle_Control_Flow() -> bool:
+    Function: function
+    for Function, FailType in ControlFlow:
+        Error:None|str = Function()
+        if Error != None:
+            print(f"ERROR: {Error}")
+        if FailType == "FailFast":
             return False
     return True
 
@@ -64,9 +93,17 @@ WindowSettings = {
     "Title": "ExoFyle",
     "FPS": 60,
 }
-Inputs = {
-    KEY_R: Load_All_Packages,
+KeyChords = {
+    "Leader": KEY_SPACE
 }
+InputTree = [
+    [KEY_R, Load_All_Packages, KeyChords["Leader"]],
+    [KEY_Q, Exit, KeyChords["Leader"]],
+]
+ControlFlow = [
+    [Build_Frame, "FailSafe"],
+    [Handle_Input, "FailSafe"],
+]
 
 
 def Entry():
@@ -77,13 +114,8 @@ def Entry():
     Load_All_Packages()
 
     while not window_should_close():
-        begin_drawing()
-        for Key in Inputs.keys():
-            if is_key_pressed(Key):
-                print("Pressed Key")
-                Inputs[Key]()
-        clear_background(Background["Color"])
-        end_drawing()
+        if Handle_Control_Flow() == False:
+            break
 
     close_window()
 
